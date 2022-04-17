37939f465f7152dccc6cb1ab68af119cebbe72c7
geoffjecrois <je.suis.geoffrey.hach@gmail.com>
2022-04-17 12:24:33 +0200

    js

---
+++ app.js
@@ -0,0 +1,97 @@
+// createCards();
+
+async function fetchData() {
+    const response = await fetch('./data.json');
+    const data = await response.json(); 
+    return data
+}
+
+async function createCards(option) {
+    const data = await fetchData();
+    const grid = document.querySelector('main');
+    
+    data.forEach(el => {
+        const name = el.title;
+        const nameClass = name.toLowerCase().replace(' ', '-');
+        const currentTime = el.timeframes[option].current;
+        const previousTime = el.timeframes[option].previous;
+        //check what to display before the previous time
+        const calcPreviousTimeText = (option) =>{
+            if (option === 'daily') return 'Yesterday';
+            if (option === 'weekly') return 'Last week';
+            if (option === 'monthly') return 'Last month';    
+        };
+        const previousTimeText = calcPreviousTimeText(option);
+
+        const section = document.createElement('section');
+        section.classList.add('card', nameClass);
+        section.innerHTML = `
+        <div class="card-illustration"></div>
+        <div class="card-content">
+        <h1>${name}</h1>
+        <button class="dots">...</button>
+        <p class="time">${currentTime}hrs</p>   
+        <p class="last-week"> ${previousTimeText} - ${previousTime}hrs</p>
+        </div>        
+        `;
+        
+        grid.append(section);
+    });
+
+};
+
+
+//styling the active timeframe btn
+let timeframeBtn = document.querySelectorAll('.timeframe-btn');
+
+timeframeBtn.forEach(el => {
+    el.addEventListener('click', () => {
+    timeframeBtn.forEach(el => el.classList.remove('active'));
+    el.classList.add('active')
+    });
+});
+
+//grab the timeframe option of the clicked btn and delete the previous cards
+timeframeBtn.forEach (el => {
+    el.addEventListener('click', () => {
+        const timeframeOption = el.dataset.option;
+        const cardsToDelete = document.querySelectorAll('.card');
+        cardsToDelete.forEach(el => el.remove());
+        createCards(timeframeOption);
+    })
+    
+});
+
+window.onload = createCards('daily');
+
+
+
+
+
+
+
+
+
+
+// console.log(fetchData());
+
+
+
+// data.forEach(activity => {
+//     const name = activity.title;
+
+//     console.log(name);
+
+// });
+
+
+
+// ` <section class="card work">
+//     <div class="card-illustration"></div>
+//     <div class="card-content">
+//     <h1></h1>
+//     <button class="dots">...</button>
+//     <p class="time">32hrs</p>   
+//     <p class="last-week">10hrs</p>
+//     </div>
+//   </section>`
